174d5781070dddfb70207ad89c9d4d8d865415aa
Saif <saifeldeenadel77@gmail.com>
2024-09-04 14:19:27 +0300

    some additions

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+DISCOUNT = 0.99
+R = 100
+
+world = np.array([[R, -1,10], [-1,-1,-1],[-1,-1,-1]])
+
+def get_q(state, action):
+  pass
+
+def get_v(state):
+  pass
+
+def value_iteration(world, actions):
+  pass
+
+def main():
+  pass
+
+if __name__=="__main__":
+  main()
+
